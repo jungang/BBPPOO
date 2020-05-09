b3f439edd8b0ada06b7914bab7092ba7e92a4cea
jun-home <jungang@vip.sina.com>
2020-05-10 04:29:21 +0800

    05.10 home

---
+++ doc/view.js
@@ -0,0 +1,21 @@
+const views = [
+  {
+    compare: true,
+    completion: true,
+    description: '当月利润分析',
+    drill__drillName: 'profit_income_expanse',
+    drill_drillName: '当月利润分析',
+    drill_name: '当月利润分析',
+    fold: false,
+    name: 'profit_income_expanse',
+    parameters: '{year,month,projectId,name}',
+    ratio: true,
+    show: true,
+    sort: false,
+    style: 'chart',
+    switch: true,
+    title: '当月利润分析'
+  }
+]
+
+console.log(views)
