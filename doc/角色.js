f1111de9ac4426e3c98ca365c860f6440c5095ec
jun <jungang@vip.sina.com>
2020-05-21 18:42:06 +0800

    0521 更新: 修复路由错误

---
+++ doc/角色.js
@@ -0,0 +1,11 @@
+const data = {
+  管理员:admin,
+  经营管理:manage_manager,
+  队列经理:project_manager,
+  班组长:group_captain,
+}
+
+console.log(data)
+//管理员 admin	经营管理 manage_manager	队列经理 project_manager	班组长 group_captain
+
+宋俊刚​ 已经新增四个用户和四个角色，管理员admin	经营管理 manage_manager	队列经理 project_manager	班组长 group_captain  密码跟用户名相同，目前每个用户对应同名的角色，但角色可以绑定多个用户，有接口可以设置。
