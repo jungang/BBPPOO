1508adfad42dc247c0cf027dea1504fb0b8f2228
LICHUN <shijiao1798@sina.com>
2020-05-25 16:53:04 +0800

    BPO-158

--- src/views/subpage/page_zl.vue
+++ src/views/subpage/page_zl.vue
@@ -62,9 +62,6 @@ export default {
         }, {
           value: 'local',
           label: '本地'
-        }, {
-          value: 'other',
-          label: '其他'
         }
       ],
       query: {
