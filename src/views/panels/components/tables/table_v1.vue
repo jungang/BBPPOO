b635232b54ae80cff9863c7d7b73059ab0676b38
jun <jungang@vip.sina.com>
2020-05-27 02:57:22 +0800

    0526 更新 undefined

--- src/views/panels/components/tables/table_v1.vue
+++ src/views/panels/components/tables/table_v1.vue
@@ -158,20 +158,20 @@ export default {
         const arrs = []
         const lirunArray = []
         this.tableData[0].dimension.forEach((item) => {
-          if(item.v_group_name){
-            item.data[0].v_group_name = item.v_group_name + '组';
-            item.data[0].v_name = item.v_group_name+ '组';
-            item.data[0].v_cu = 'jiacu';
-            arrs.push(item.data[0]);
-          }else{
+          if (item.v_group_name) {
+            item.data[0].v_group_name = item.v_group_name + '组'
+            item.data[0].v_name = item.v_group_name + '组'
+            item.data[0].v_cu = 'jiacu'
+            arrs.push(item.data[0])
+          } else {
             item.data.forEach((_item) => {
               if (_item.v_id) {
                 employeeList.forEach((__item) => {
                   __item.children.forEach((___item) => {
-                    if(_item.v_id === ___item.value){
-                      _item['v_name'] = ___item.label;
-                      _item['v_group_name'] = __item.label;
-                      _item['v_cu'] = '';
+                    if (_item.v_id === ___item.value) {
+                      _item['v_name'] = ___item.label
+                      _item['v_group_name'] = __item.label
+                      _item['v_cu'] = ''
                     }
                   })
                 })
