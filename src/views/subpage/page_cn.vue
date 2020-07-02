4ac6daf565a06da3949beb3dbe9396c22f3e7dc4
LICHUN <shijiao1798@sina.com>
2020-07-02 10:02:48 +0800

    修改对比图，日期选择联动首页

--- src/views/subpage/page_cn.vue
+++ src/views/subpage/page_cn.vue
@@ -119,7 +119,7 @@ export default {
         dateType: 'month', // 天 day | 周 week | 月 month | 年 year
         date: new Date(), // 日期20200501
         multiple: true,
-        isStore: 'duibi',
+        isStore: 'false',
         group: 'null', // 选择组信息
         type: this.$store.state.user.alias === 'ts' ? false : 'all' // 合集 all | 集团 group | 本地 local
       }
